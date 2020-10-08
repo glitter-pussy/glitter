7ff6c34ecd5989e386813e2a38cf41ec0c4b0921
glitter-pussy <siiiiiiiiiiya@gmail.com>
2020-10-08 14:13:16 +0500

    нахождение параметров

---
+++ вычисление заданных величин/вычисление заданных величин/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace вычисление_заданных_величин
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, n, k;
+            Console.WriteLine("введите стороны прямоугольника:");
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            n = 2 * (a + b);
+            Console.WriteLine("периметр прямоугольника:" + n);
+            k = a * b;
+            Console.WriteLine("площадь прямоугольника:" + k);
+            c = Math.Sqrt(a * a + b * b);
+            Console.WriteLine("длина диагонали прямоугольника:" + c);
+
+            Console.ReadKey();
+        }
+    }
+}
