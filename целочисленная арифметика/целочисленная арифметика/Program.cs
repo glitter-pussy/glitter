d0a0f1fd8be86cd54ab6f78e85e34f5dac28fdcd
glitter-pussy <siiiiiiiiiiya@gmail.com>
2020-10-11 16:06:56 +0500

    нахождение одного числа по другому

---
+++ целочисленная арифметика/целочисленная арифметика/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace целочисленная_арифметика
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, n, x;
+            Console.WriteLine("Введите трехзначное число:");
+            n = int.Parse(Console.ReadLine());
+            a = n / 100;
+            b = (n / 10) % 10;
+            c = n % 10;
+            x = 100 * a + 10 * c + b;
+            Console.WriteLine();
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
