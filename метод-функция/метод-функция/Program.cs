eb115ef967f5726f26fbcb9fc91e4be4d33e1bff
glitter-pussy <siiiiiiiiiiya@gmail.com>
2020-10-17 22:56:33 +0500

    аой

---
+++ метод-функция/метод-функция/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace метод_функция
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = GetValue();
+            Console.WriteLine("x=" + x);
+            Console.ReadKey();
+        }
+        static double GetValue()
+        {
+            return F(3, 2) + F(5, 3) + F(11, 7);
+        }
+        static double F(double a, double b)
+        {
+            return (Math.Sqrt(a) - Math.Sqrt(b)) / (Math.Sqrt(a) + Math.Sqrt(b));
+        }
+    }
+}
