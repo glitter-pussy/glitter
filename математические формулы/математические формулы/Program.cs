2e877c3363a8324b50f2b9d52cf6438ec43dfa4e
glitter-pussy <siiiiiiiiiiya@gmail.com>
2020-10-17 22:42:36 +0500

    считает в радианах

---
+++ математические формулы/математические формулы/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace математические_формулы
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Введите значение х:");
+            var x = double.Parse(Console.ReadLine());
+            var y = F(x);
+            Console.WriteLine("f(x) =" + y);
+            Console.ReadKey();
+        }
+        static double F(double x)
+        {
+            return (Math.Abs(x) + 2 * Math.Sin(Math.Pow(Math.Tan(x),2) + 4)) / ( 5.5 * x);
+        }
+    }
+}
