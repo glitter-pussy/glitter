8d57c677c3db052ec72843c203a6bbe8c2463235
glitter-pussy <siiiiiiiiiiya@gmail.com>
2020-10-27 22:24:28 +0500

    буквы

---
+++ методы стринг/методы стринг/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace методы_стринг
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "перемена";
+            Console.WriteLine(s1);
+            Console.WriteLine(s1.Substring(4, 2) + s1.Substring(2, 1) + s1.Substring(7, 1));
+            Console.WriteLine(s1.Substring(6, 1) + s1.Substring(1, 2) + s1.Substring(0, 1) + s1.Substring(7, 1));
+            Console.ReadKey();
+                } } }
