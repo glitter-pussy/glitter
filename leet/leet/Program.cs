21a5f29917533ee690acd9dabbf2d9345d34a9ce
glitter-pussy <siiiiiiiiiiya@gmail.com>
2020-10-28 15:28:39 +0500

    jq

--- leet/leet/Program.cs
+++ leet/leet/Program.cs
@@ -14,15 +14,15 @@ namespace leet
 
         {
             Console.WriteLine("Введите текст на английском:");
-            string x = F();
-            Console.WriteLine("The final string:{0}", x);
+            string x = Console.ReadLine();
+            x = x.ToUpper();
+            Console.WriteLine($"The final string:{F(x)}");
             Console.ReadKey();
         }
 
-        static string F()
+        static string F(string x)
         {
-            string x = Console.ReadLine();
-            x = x.ToUpper();
+           
             return x.Replace("A", "4").Replace("B","8").Replace("C", "(").Replace("D", "|)").Replace("E", "3").Replace("F", "|=").Replace("G", "6").Replace("H", "|-|").Replace("I", "!").Replace("J", ")").Replace("K", "|<").Replace("L", "1").Replace("M", "|\\//|").Replace("N", "|\\|").Replace("O", "0").Replace("P", "|>").Replace("Q", "9").Replace("R", "|2").Replace("S", "5").Replace("T", "7").Replace("U", "|_|").Replace("V", "\\//").Replace("W", "\\//\\//").Replace("X", "><").Replace("Y", "'/").Replace("Z", "2");
         }
        
