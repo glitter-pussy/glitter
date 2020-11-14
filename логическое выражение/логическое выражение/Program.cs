da1a29a44f8a46859ba911ed0f5a2672e3ab440b
glitter-pussy <siiiiiiiiiiya@gmail.com>
2020-11-14 06:25:42 +0500

    вычисляет логическое выражение

---
+++ логическое выражение/логическое выражение/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace логическое_выражение
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var k = GetNumber("k");
+            var m = GetNumber("m");
+            var n = GetNumber("n");
+
+            string answer = "Нет.";
+
+            if (EvaluateLogicalExpression(k, m, n))
+                answer = "Да.";
+
+            Console.WriteLine("Верно ли, что только одно из чисел k, m, или n больше 15? " +
+                answer);
+
+            Console.ReadKey();
+        }
+
+        static bool EvaluateLogicalExpression(int k, int m, int n)
+        {
+            return k > 15 ^ m > 15 ^ n > 15;
+        }
+
+        static int GetNumber(string numberName)
+        {
+            Console.WriteLine("Введите число " + numberName);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
