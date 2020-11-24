ec928eabc3fa5240530930cda8682e802a46d5ac
glitter-pussy <siiiiiiiiiiya@gmail.com>
2020-11-24 12:43:00 +0500

    считает функцию

---
+++ функция/функция/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace функция
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение x");
+          double x = double.Parse(Console.ReadLine());
+            if (x < -1)
+            {
+                Console.WriteLine(" y = 1");
+            }
+            else if (-1 <= x && x <= 1)
+            {
+                Console.WriteLine(" y = " + GetYMiddle(x));
+            }
+            else
+            {
+                Console.WriteLine(" y = " + GetYUpper(x));
+            }
+            Console.ReadKey();
+        }
+
+           
+        static double GetYMiddle(double x)
+        {
+            return x * x;
+        }
+        static double GetYUpper(double x)
+        {
+            return 2 - x;
+        }
+
+    }
+}
